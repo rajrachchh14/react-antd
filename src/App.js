3e07b7b6e710b738b4e5c7908cc2c1ef0d2d8193
rajrachchh14 <rajrachchh14@gmail.com>
2022-02-06 21:24:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+import { DatePicker } from 'antd';
+
+export default function App() {
+  return (
+    <div>
+      <DatePicker />
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
