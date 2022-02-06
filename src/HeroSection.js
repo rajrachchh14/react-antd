d1b6013aeed1cc7f7b1f0c742af0494fb689bf08
rajrachchh14 <rajrachchh14@gmail.com>
2022-02-06 22:57:58 +0530

    1 file modified

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Carousel } from 'antd';
 
 function HeroSection() {
-  return <div>Hero</div>;
+  const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
+  return (
+    <div>
+      <Carousel>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default HeroSection;
