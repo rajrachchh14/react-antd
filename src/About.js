e48c156c3015ff7064e741665fa33199a1036401
rajrachchh14 <rajrachchh14@gmail.com>
2022-02-06 23:48:56 +0530

    1 file modified

--- src/About.js
+++ src/About.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="container-fluid">
+      <Row gutter={[16, 16]}>
+        <Col span={8}>col-8</Col>
+        <Col span={8}>col-8</Col>
+        <Col span={8}>col-8</Col>
+      </Row>
+    </div>
+  );
 }
 
 export default About;
